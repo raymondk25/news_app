6c8d8745c7bff447638ccdd7a9ccf40a1d0d5723
Raymond <raymondk25@users.noreply.github.com>
2023-07-18 14:11:53 +0800

    Enhance the get all news method with url segmentation

---
+++ lib/consts/api_consts.dart
@@ -0,0 +1,2 @@
+String kBaseUrl = "newsapi.org";
+String kApiKey = "39bcc9134cd84749b9ae30dbb6bdea28";
--- lib/services/news_api.dart
+++ lib/services/news_api.dart
@@ -1,15 +1,21 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:http/http.dart' as http;
+import 'package:news_app/consts/api_consts.dart';
 import 'package:news_app/models/news_model.dart';
 
 class NewsAPIServices {
   static Future<List<NewsModel>> getAllNews() async {
-    var url =
-        Uri.parse("https://newsapi.org/v2/everything?q=bitcoin&pageSize=5&apiKey=39bcc9134cd84749b9ae30dbb6bdea28");
-    var response = await http.get(url);
+    var uri = Uri.https(kBaseUrl, "v2/everything", {
+      "q": "bitcoin",
+      "pageSize": "5",
+      "domains": "bbc.co.uk,techcrunch.com,engadget.com",
+    });
+    var response = await http.get(uri, headers: {"X-Api-Key": kApiKey});
 
     Map data = jsonDecode(response.body);
+    // log(data.toString());
     List newsTempList = [];
     for (var v in data["articles"]) {
       newsTempList.add(v);
